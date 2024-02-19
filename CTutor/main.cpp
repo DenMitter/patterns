5636b68f60a069fbc5b1625273b4490a029a0b01
DenMitter <57758gfk@gmail.com>
2024-02-19 20:38:57 +0200

    add hw ex 3

--- CTutor/main.cpp
+++ CTutor/main.cpp
@@ -1,83 +1,122 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
 #include <Windows.h>
 
-class ProductFactory {
+class ObjectBuilder {
 public:
-    virtual ~ProductFactory() {}
-    virtual void createClothing(std::string type) = 0;
-    virtual void createElectronics(std::string type) = 0;
+    virtual ~ObjectBuilder() {}
+    virtual void addComponent(std::string type, std::string parameters) = 0;
+    virtual void printObject() = 0;
 };
 
-class ClothingFactory : public ProductFactory {
+class Component1 : public ObjectBuilder {
 public:
-    void createClothing(std::string type) override {
-        if (type == "футболка") {
-            std::cout << "Створено футболку\n";
+    Component1(std::string parameters) : parameters_(parameters) {}
+
+    void addComponent(std::string type, std::string parameters) override {
+        std::cout << "Компонент1 не має додаткових компонентів\n";
+    }
+
+    void printObject() override {
+        std::cout << "Компонент1 з параметрами: " << parameters_ << std::endl;
+    }
+
+private:
+    std::string parameters_;
+};
+
+class Component2 : public ObjectBuilder {
+public:
+    Component2(std::string parameters) : parameters_(parameters) {}
+
+    void addComponent(std::string type, std::string parameters) override {
+        std::cout << "Компонент2 не має додаткових компонентів\n";
+    }
+
+    void printObject() override {
+        std::cout << "Компонент2 з параметрами: " << parameters_ << std::endl;
+    }
+
+private:
+    std::string parameters_;
+};
+
+class Component3 : public ObjectBuilder {
+public:
+    Component3(std::string parameters) : parameters_(parameters) {}
+
+    void addComponent(std::string type, std::string parameters) override {
+        std::cout << "Компонент3 не має додаткових компонентів\n";
+    }
+
+    void printObject() override {
+        std::cout << "Компонент3 з параметрами: " << parameters_ << std::endl;
+    }
+
+private:
+    std::string parameters_;
+};
+
+class ObjectDirector {
+public:
+    ObjectDirector(ObjectBuilder* builder) : builder_(builder) {}
+
+    void constructObject() {
+        builder_->addComponent("Компонент1", "Параметри для компонента 1");
+        builder_->addComponent("Компонент2", "Параметри для компонента 2");
+        builder_->addComponent("Компонент3", "Параметри для компонента 3");
+    }
+
+private:
+    ObjectBuilder* builder_;
+};
+
+class ConcreteObjectBuilder : public ObjectBuilder {
+public:
+    void addComponent(std::string type, std::string parameters) override {
+        if (type == "Компонент1") {
+            components_.push_back(new Component1(parameters));
         }
-        else if (type == "штани") {
-            std::cout << "Створено штани\n";
+        else if (type == "Компонент2") {
+            components_.push_back(new Component2(parameters));
         }
-        else if (type == "куртка") {
-            std::cout << "Створено куртку\n";
+        else if (type == "Компонент3") {
+            components_.push_back(new Component3(parameters));
         }
         else {
-            std::cout << "Такий тип одягу не підтримується\n";
+            std::cout << "Не вдалося додати компонент: невідомий тип\n";
         }
     }
 
-    void createElectronics(std::string type) override {
-        std::cout << "Фабрика одягу не може створити електроніку\n";
+    void printObject() override {
+        std::cout << "Об'єкт містить компоненти:\n";
+        for (auto* component : components_) {
+            component->printObject();
+        }
     }
+
+    ~ConcreteObjectBuilder() {
+        for (auto* component : components_) {
+            delete component;
+        }
+    }
+
+private:
+    std::vector<ObjectBuilder*> components_;
 };
-
-class ElectronicsFactory : public ProductFactory {
-public:
-    void createClothing(std::string type) override {
-        std::cout << "Фабрика електроніки не може створити одяг\n";
-    }
-
-    void createElectronics(std::string type) override {
-        if (type == "смартфон") {
-            std::cout << "Створено смартфон\n";
-        }
-        else if (type == "ноутбук") {
-            std::cout << "Створено ноутбук\n";
-        }
-        else if (type == "телевізор") {
-            std::cout << "Створено телевізор\n";
-        }
-        else {
-            std::cout << "Такий тип електроніки не підтримується\n";
-        }
-    }
-};
-
-class Tshirt {};
-class Pants {};
-class Jacket {};
-
-class Smartphone {};
-class Laptop {};
-class Television {};
 
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
 
-    ProductFactory* clothingFactory = new ClothingFactory();
-    ProductFactory* electronicsFactory = new ElectronicsFactory();
+    ObjectBuilder* builder = new ConcreteObjectBuilder();
+    ObjectDirector director(builder);
 
-    clothingFactory->createClothing("футболка");
-    clothingFactory->createClothing("штани");
-    clothingFactory->createClothing("куртка");
+    director.constructObject();
+    builder->printObject();
 
-    electronicsFactory->createElectronics("смартфон");
-    electronicsFactory->createElectronics("ноутбук");
-    electronicsFactory->createElectronics("телевізор");
-
-    delete clothingFactory;
-    delete electronicsFactory;
+    delete builder;
 
     return 0;
 }
